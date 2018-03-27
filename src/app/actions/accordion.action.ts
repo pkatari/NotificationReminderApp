144dcbbf578f1305efa2afdec40161a9e60db8e2
Priyanka Kataria <pkataria2@sapient.com>
2018-03-27 11:11:00 +0530

    tslint error removed

--- src/app/actions/accordion.action.ts
+++ src/app/actions/accordion.action.ts
@@ -1,33 +1,32 @@
-import { Action } from '@ngrx/store'; 
+import { Action } from '@ngrx/store';
 import { AccordionState } from '../model/accordion.state';
 
 export const ACCORDION_SETTINGS_UPDATE = '[ACCORDION] Update aAccordion Settings';
 export const ACCORDION_SETTINGS_UPDATE_SUCCESS = '[ACCORDION] Update Accordion Settings Success';
 export const ACCORDION_SETTINGS_UPDATE_FAIL = '[ACCORDION] Update Accordion Settings Fail';
 
-//This action is used to update accordion notification data
+// This action is used to update accordion notification data
 export class UpdateAccordionData implements Action {
-    readonly type = ACCORDION_SETTINGS_UPDATE
-    constructor(public payload:AccordionState[]){
-        console.log("Action Update Accordion Data");
-    } 
-} 
+    readonly type = ACCORDION_SETTINGS_UPDATE;
+    constructor(public payload: AccordionState[]) {
+        console.log('Action Update Accordion Data');
+    }
+}
 
-/*This action is dispatched by effect updateAccordionData once service 
+/* This action is dispatched by effect updateAccordionData once service
 successfully saves data */
 export class UpdateAccordionDataSuccess implements Action {
-    readonly type = ACCORDION_SETTINGS_UPDATE_SUCCESS 
-    constructor(public payload: AccordionState[]){
-        console.log("Action Update Accordion Data Success");
-    } 
-} 
+    readonly type = ACCORDION_SETTINGS_UPDATE_SUCCESS;
+    constructor(public payload: AccordionState[]) {
+        console.log('Action Update Accordion Data Success');
+    }
+}
 
-//This action is dispatched by effect updateAccordionData in case there is error
+// This action is dispatched by effect updateAccordionData in case there is error
 export class UpdateAccordionDataError implements Action {
-    readonly type = ACCORDION_SETTINGS_UPDATE_FAIL 
-    constructor(payload: {}){
-        console.log("Action Update Accordion Data Error");
-    } 
- 
-} 
+    readonly type = ACCORDION_SETTINGS_UPDATE_FAIL;
+    constructor(payload: {}) {
+        console.log('Action Update Accordion Data Error');
+    }
+}
 export type AccordionAll = UpdateAccordionData | UpdateAccordionDataSuccess | UpdateAccordionDataError;
--- src/app/actions/global.action.ts
+++ src/app/actions/global.action.ts
@@ -7,34 +7,33 @@ export const GlOBAL_SETTINGS_UPDATE_SUCCESS = '[GlOBAL] Update Global Settings S
 export const GlOBAL_SETTINGS_UPDATE_FAIL = '[GlOBAL] Update Global Settings Fail';
 
 export class GetGlobalApplySettings implements Action {
-    readonly type = APPLY_GLOBAL_SETTINGS; 
+    readonly type = APPLY_GLOBAL_SETTINGS;
     constructor(public payload: GetGlobalSettings[]) {
-       console.log("Action GetGlobalApplySettings");
-    };
-} 
+       console.log('Action GetGlobalApplySettings');
+    }
+}
 
-//This action is dispatched when'Apply' button is clicked.
+// This action is dispatched when'Apply' button is clicked.
 export class UpdateGlobalSettings implements Action {
-    readonly type = GlOBAL_SETTINGS_UPDATE
-    constructor(public payload: GlobalUpdateState[]){
-        console.log("Action Update Global settings");
-    } 
-} 
+    readonly type = GlOBAL_SETTINGS_UPDATE;
+    constructor(public payload: GlobalUpdateState[]) {
+        console.log('Action Update Global settings');
+    }
+}
 
-/*This action is dispatched by effect updateGlobalSetting once service 
+/* This action is dispatched by effect updateGlobalSetting once service 
 successfully saves data */
 export class UpdateGlobalSettingsSuccess implements Action {
-    readonly type = GlOBAL_SETTINGS_UPDATE_SUCCESS  
-    constructor(public payload: GlobalUpdateState[]){
-        console.log("Action UpdateGlobalSettingsSuccess");
-    } 
-} 
-//This action is dispatched by effect updateGlobalSetting in case there is error
+    readonly type = GlOBAL_SETTINGS_UPDATE_SUCCESS;
+    constructor(public payload: GlobalUpdateState[]) {
+        console.log('Action UpdateGlobalSettingsSuccess');
+    }
+}
+// This action is dispatched by effect updateGlobalSetting in case there is error
 export class UpdateGlobalSettingsError implements Action {
-    readonly type = GlOBAL_SETTINGS_UPDATE_FAIL
-    constructor(payload: {}){
-        console.log("Action UpdateGlobalSettingsError");
-    } 
- 
-} 
+    readonly type = GlOBAL_SETTINGS_UPDATE_FAIL;
+    constructor(payload: {}) {
+        console.log('Action UpdateGlobalSettingsError');
+    }
+}
 export type GlobalAll = GetGlobalApplySettings | UpdateGlobalSettings  | UpdateGlobalSettingsSuccess  | UpdateGlobalSettingsError;
