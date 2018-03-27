6bcd97fee6bd1f4ac7492ca6c77bb58225984773
Priyanka Kataria <pkataria2@sapient.com>
2018-03-27 11:43:06 +0530

    TS lint errors

--- src/app/service/accordiondata.service.ts
+++ src/app/service/accordiondata.service.ts
@@ -1,29 +1,23 @@
-import {Injectable} from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs/Observable";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 import "rxjs/Rx";
-import { AccordionState } from "../model/accordion.state";
- 
+import { HttpClient } from '@angular/common/http';
+import { AccordionState } from '../model/accordion.state';
 @Injectable()
 export class AccordionDataService {
-    accData : {}
+    accData: {};
     private accDataGlobal = '../../assets/accordionstate.json';
     constructor(private http: HttpClient) {
     }
- 
      getAccordionData(): Observable<any> {
          return this.http.get(this.accDataGlobal);
-        
     }
 
-    updateAcordionData(globaldata : {}) : Observable<{}> {
-        console.log("Inside Service..Update Accordion Data")
+    updateAcordionData(globaldata: {}): Observable<{}> {
        this.accData =globaldata;
        console.log(this.accData);
-       console.log("Data is Updated..........");
        return Observable.of(this.accData);
     }
- 
     private handleError(error: Response) {
          return Observable.throw(error.statusText);
     }
--- src/app/service/globaldata.service.ts
+++ src/app/service/globaldata.service.ts
@@ -1,28 +1,23 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/Observable";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 import "rxjs/Rx";
 import { HttpClient } from '@angular/common/http';
-import { GetGlobalSettings } from "../model/globalsettings.model";
- 
+import { GetGlobalSettings } from '../model/globalsettings.model';
 @Injectable()
 export class GlobalDataService {
-    globalData : {};
+    globalData: {};
     private dataGlobal = '../../assets/globalsetting.json';
     constructor(private http: HttpClient) {
     }
- 
     getGlobalData(): Observable<any> {
         return this.http.get(this.dataGlobal);
     }
 
-    updateGlobalData(globaldata : {}) : Observable<{}> {
-        console.log("Inside Service..Update Global Data")
+    updateGlobalData(globaldata: {}): Observable<{}> {
        this.globalData =globaldata;
        console.log(this.globalData);
-       console.log("Data is Updated..........");
-       return Observable.of(this.globalData);;
+       return Observable.of(this.globalData);
     }
- 
     private handleError(error: Response) {
         return Observable.throw(error.statusText);
     }
