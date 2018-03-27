23537a45ee5a17c4fa66a16791d73d646dc10c4e
Priyanka Kataria <pkataria2@sapient.com>
2018-03-27 11:36:28 +0530

    TS lint error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccordionDataService } from './service/accordiondata.service';
 import { GlobalsettingsComponent } from './globalsettings/globalsettings.component';
 import { NotifaccordionComponent } from './notifaccordion/notifaccordion.component';
-import { GlobalDataService }  from './service/globaldata.service';
+import { GlobalDataService } from './service/globaldata.service';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -34,7 +34,7 @@ import { ObjectIterable } from './pipe/objectiterator';
     EffectsModule.forRoot([DataBaseEffects]),
     NgbModule.forRoot()
   ],
-  providers: [AccordionDataService,GlobalDataService],
+  providers: [AccordionDataService, GlobalDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
